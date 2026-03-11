7c67d0dfa5cb4291f8539deca8d7601558e45220
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-11 10:28:45 +0100

    Add SQL queries for degrees and subjects statistics

--- queries/queries.sql
+++ queries/queries.sql
@@ -112,10 +112,18 @@ GROUP BY g.nombre
 ORDER BY total DESC;
 
 -- 21. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades. (grau, total)
-
+SELECT g.nombre AS grau, COUNT(a.id) AS total
+FROM grado g
+LEFT JOIN asignatura a ON g.id = a.id_grado
+GROUP BY g.nombre
+HAVING COUNT(a.id) > 40
+ORDER BY total DESC;
 
 -- 22. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus. (grau, tipus, total_creditos)
-
+SELECT g.nombre AS grau, a.tipo AS tipus, SUM(a.creditos) AS total_creditos
+FROM grado g
+JOIN asignatura a ON g.id = a.id_grado
+GROUP BY grau, tipus;
 
 -- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats. (anyo_inicio, total)
 
